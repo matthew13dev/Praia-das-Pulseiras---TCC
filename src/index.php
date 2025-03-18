fcfc7b154f80140fc1d13c702445d7fc0a5a03f8
matthew13 <matthew.coder@outlook.com>
2025-03-18 13:07:09 -0300

    changes on index.php

--- src/index.php
+++ src/index.php
@@ -29,17 +29,74 @@
 
 <body>
 
-    <a class="whatsapp-button" href="https://wa.me/SEU_NUMERO" target="_blank">
+    <a class="whatsapp-button" href="https://wa.me/13996893769" target="_blank">
         <img src="images/whatsapp1.png" alt="WhatsApp">
     </a>
 
-    <div class="container">
-        <h1>Praia das Pulseiras</h1>
-        <h3>Mais que uma loja virtual!</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor nulla repudiandae facere? Quos natus aspernatur dicta quod recusandae, nihil, ipsum ut saepe facilis repellat omnis placeat. Molestiae voluptas consequatur aut?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor nulla repudiandae facere? Quos natus aspernatur dicta quod recusandae, nihil, ipsum ut saepe facilis repellat omnis placeat. Molestiae voluptas consequatur aut?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor nulla repudiandae facere? Quos natus aspernatur dicta quod recusandae, nihil, ipsum ut saepe facilis repellat omnis placeat. Molestiae voluptas consequatur aut?</p>
-    </div>
+    <section id="home">
+        <div class="container">
+            <h1>Praia das Pulseiras 🌊✨</h1>
+            <h2>Mais que uma loja, um estilo de vida!</h2>
+            <p>Bem-vindo à Praia das Pulseiras, o lugar perfeito para quem ama acessórios únicos e feitos à mão! Nossas bijuterias artesanais são inspiradas na beleza do mar, na leveza da praia e na autenticidade de cada detalhe.</p>
+            <p>Cada peça é cuidadosamente criada com materiais de alta qualidade, garantindo não apenas beleza, mas também durabilidade. Seja para um look casual ou um toque especial no seu dia, temos algo feito especialmente para você!</p>
+            <p>Descubra nossa coleção e leve um pedacinho do encanto artesanal com você. 💙</p>
+            <a href="#catalogo" class="btn">Ver Produtos</a>
+        </div>
+    </section>
+        
+
+    <section class="catalogo ">
+        <div class="container">
+        <h1 class="section--ttitle">Catálogo de Produtos</h1>    
+        <div class="catalogo--grid">
+            
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            <div class="card">
+                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
+                <h1>Product`s Name</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+
+                <a href="">Adicinar a Sacola</a>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    
 </body>
 
 </html>
