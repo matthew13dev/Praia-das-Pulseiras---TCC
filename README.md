32232300697f4e302738619257393f24cb64d12a
matthew13 <matthew.coder@outlook.com>
2025-05-14 00:12:08 -0300

    diretório de pastas reorganizado

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 Projeto para TCC (Trabalho de Conclusão de Curso)
 **Praia das Pulseiras**
-Em 02/03/25 Enviamos a (DECLARAÇÃO DE VISÃO DO PROJETO - AGENDA_01 PTCC);
-Em 15/03/25 Enviamos a (INTRODUÇÃO DP PROJETO - AGENDA_02 PTCC);
+ 
--- src/index.php
+++ src/index.php
@@ -37,66 +37,88 @@
         <div class="container">
             <h1>Praia das Pulseiras 🌊✨</h1>
             <h2>Mais que uma loja, um estilo de vida!</h2>
-            <p>Bem-vindo à Praia das Pulseiras, o lugar perfeito para quem ama acessórios únicos e feitos à mão! Nossas bijuterias artesanais são inspiradas na beleza do mar, na leveza da praia e na autenticidade de cada detalhe.</p>
-            <p>Cada peça é cuidadosamente criada com materiais de alta qualidade, garantindo não apenas beleza, mas também durabilidade. Seja para um look casual ou um toque especial no seu dia, temos algo feito especialmente para você!</p>
+            <p>Bem-vindo à Praia das Pulseiras, o lugar perfeito para quem ama acessórios únicos e feitos à mão! Nossas
+                bijuterias artesanais são inspiradas na beleza do mar, na leveza da praia e na autenticidade de cada
+                detalhe.</p>
+            <p>Cada peça é cuidadosamente criada com materiais de alta qualidade, garantindo não apenas beleza, mas
+                também durabilidade. Seja para um look casual ou um toque especial no seu dia, temos algo feito
+                especialmente para você!</p>
             <p>Descubra nossa coleção e leve um pedacinho do encanto artesanal com você. 💙</p>
             <a href="#catalogo" class="btn">Ver Produtos</a>
         </div>
     </section>
-        
+
 
     <section class="catalogo ">
         <div class="container">
-        <h1 class="section--ttitle">Catálogo de Produtos</h1>    
-        <div class="catalogo--grid">
-            
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+            <h1 class="section--ttitle">Catálogo de Produtos</h1>
+            <div class="catalogo--grid">
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+                    <a href="">Adicinar a Sacola</a>
+                </div>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+                    <a href="">Adicinar a Sacola</a>
+                </div>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+                    <a href="">Adicinar a Sacola</a>
+                </div>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
-            <div class="card">
-                <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp" alt="Photo Product">
-                <h1>Product`s Name</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate neque commodi, a sit dolore id facere.</p>
+                    <a href="">Adicinar a Sacola</a>
+                </div>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
 
-                <a href="">Adicinar a Sacola</a>
-            </div>
+                    <a href="">Adicinar a Sacola</a>
+                </div>
+                <div class="card">
+                    <img src="https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/1049aaaa58142abf17cf0e607b233127.jpg?imageView2/2/w/800/q/70/format/webp"
+                        alt="Photo Product">
+                    <h1>Product`s Name</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero voluptatum beatae distinctio
+                        blanditiis ex doloribus placeat, atque, nobis ipsum temporibus praesentium deserunt cupiditate
+                        neque commodi, a sit dolore id facere.</p>
+
+                    <a href="">Adicinar a Sacola</a>
+                </div>
             </div>
         </div>
     </section>
 
-    
+
 </body>
 
 </html>
